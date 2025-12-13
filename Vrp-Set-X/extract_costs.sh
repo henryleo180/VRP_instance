fb2658e8857d2c0e2452f505f358ea6574bb05ee
Heinrich <113572382+henryleo180@users.noreply.github.com>
2025-12-13 14:56:19 -0500

    Initial

---
+++ Vrp-Set-X/extract_costs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /blue/yu.yang1/haoran.liu/VRPinstance/Vrp-Set-X/X
+for sol in *.sol; do
+    name="${sol%.sol}"
+    cost=$(tail -1 "$sol" | grep -o '[0-9]\+')
+    vehicles=$(grep -c "^Route #" "$sol")
+    echo "$name $cost $vehicles"
+done | sort > ../best_solutions.txt
